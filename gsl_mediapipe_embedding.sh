bf997a6ee3ede68c41c17c301bd54e42916c723e
Kevin De Keyser <dekevin@cnb-d102-18.inf.ethz.ch>
2021-07-21 12:32:10 +0200

    more embeddings

---
+++ gsl_mediapipe_embedding.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 main2.py --config ./config/gsl_mediapipe/embed_val_joint.yaml --work-dir pretrain_eval/gsl_mediapipe_c3/joint --half --amp-opt-level 2  --batch-size 16 --forward-batch-size 8 --device 0
