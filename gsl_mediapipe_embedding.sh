81d660039c96a20104a8c7fffbaf98bb45080c65
Kevin De Keyser <dekevin@cnb-d102-18.inf.ethz.ch>
2021-08-14 01:29:23 +0200

    added embeddings bg

--- gsl_mediapipe_embedding.sh
+++ gsl_mediapipe_embedding.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-python3 main2.py --config ./config/gsl_mediapipe/embed_val_joint.yaml --work-dir pretrain_eval/gsl_mediapipe_c2/joint --half --amp-opt-level 2  --batch-size 16 --forward-batch-size 8 --device 0
+# python3 main2.py --config ./config/gsl_mediapipe/embed_val_joint.yaml --work-dir pretrain_eval/gsl_mediapipe_c2/joint --half --amp-opt-level 2  --batch-size 16 --forward-batch-size 8 --device 0
+
+python3 main2.py --config ./config/gsl_mediapipe/embed_embed_joint.yaml --work-dir pretrain_eval/gsl_mediapipe_wbg_newmethod --half --amp-opt-level 2  --batch-size 16 --forward-batch-size 8 --device 0
