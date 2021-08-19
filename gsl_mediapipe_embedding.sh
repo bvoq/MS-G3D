9875506442049d5d09d8dcc44fea9520e103185f
bvoq <dekevin@cnb-d102-18.inf.ethz.ch>
2021-08-19 21:00:50 +0200

    added bgclass

--- gsl_mediapipe_embedding.sh
+++ gsl_mediapipe_embedding.sh
@@ -3,4 +3,4 @@
 # python3 main2.py --config ./config/gsl_mediapipe/embed_val_joint.yaml --work-dir pretrain_eval/gsl_mediapipe_c2/joint --half --amp-opt-level 2  --batch-size 16 --forward-batch-size 8 --device 0
 #gsl_mediapipe_wbg_newmethod
 
-python3 main2.py --config ./config/gsl_mediapipe/embed_embed_joint.yaml --work-dir pretrain_eval/gsl_mediapipe_wbg_newmethod --half --amp-opt-level 2  --batch-size 16 --forward-batch-size 8 --device 0
+python3 main2.py --config ./config/gsl_mediapipe/embed_embed_joint.yaml --work-dir pretrain_eval/gsl_mediapipe_wbgasclass --half --amp-opt-level 2  --batch-size 16 --forward-batch-size 8 --device 0
